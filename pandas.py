38b9f2aa533adda6dd3aec597cc30bacb280d8e9
Jacson Feiten <jacsonfeiten95@gmail.com>
2021-03-12 01:12:45 -0300

    logistic regression added
    logistic regression, export dataset as pickle, what levels of the variable mean

--- pandas.py
+++ pandas.py
@@ -1,4 +1,32 @@
+# Learning pandas
+
+# Dataset's variables
+
+# Age
+#   (1) younger than 18 years
+#   (2) 18 - 26 years
+#   (3) 27 - 35 years
+#   (4) 36 - 44 years
+#   (5) 45 - 53 years
+#   (6) 54 - 62 years
+#   (7) 63 - 71 years
+
+# Education
+#   1, 2: Primary
+#   3, 4: Secondary
+#   5, 6, 7: Tertiary
+
+# Income
+#   From 0 to R$1.254,00
+#   From R$1.255,00 to R$2.004,00
+#   From R$2.005,00 to R$8.640,00
+#   From R$8.641,00 to R$11.261,00
+#   R$11.262,00 or more
+
+# Load modules
 import pandas as pd
+
+# Load dataset
 df = pd.read_csv('data/dataset.csv')
 df.head
 df.shape
@@ -17,6 +45,10 @@ df['Sex'] = df['Sex'].replace('Mulher', 'Woman')
 
 df['Consumption'] = df['Consumption'].replace(['Sim', 'Não'], ['Yes', 'No'])
 df
+
+# Exporting dataset
+df.to_pickle("data/df.pkl")
+
 # Selecting columns
 df['Age']
 df[['Age', 'Education']]
